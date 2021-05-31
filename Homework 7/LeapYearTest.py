55cbfe199394f9818e2dd3a6febfad089838991b
bhaveesh09 <72036215+bhaveesh09@users.noreply.github.com>
2021-05-30 17:00:33 -0700

    LeapYear Program Negative number check

--- Homework 7/LeapYearTest.py
+++ Homework 7/LeapYearTest.py
@@ -1,14 +1,11 @@
-from LeapYearProgram import leapYear
+from Programs import LeapYear
 import unittest
-
 
 class testLeapYear(unittest.TestCase):
     def test_leapYear(self):
-        case = leapYear(-2000)
+        case = LeapYear(-2000)
         self.assertEqual(case, False)
 
-
-
-
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
+ 
---
+++ Homework 7/Programs.py
@@ -0,0 +1,10 @@
+
+from ast import Pass
+
+
+def LeapYear(x):
+    if (x<0):                #Now check if user input is a positive number
+        Pass
+        
+
+
