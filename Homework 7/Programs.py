58cea928c2ba14c42be8716ecdab1d0829010d30
Saibhaveesh Beemireddy <beemires@flip1.engr.oregonstate.edu>
2021-05-31 10:32:23 -0700

    LeapYear Negative number Condition Pass

--- Homework 7/Programs.py
+++ Homework 7/Programs.py
@@ -1,9 +1,11 @@
 
+from ast import Pass
 
 def LeapYear(x):
     if (x<0):                #Now check if user input is a positive number
-        return False
+        return Pass
         
+
 
     
 
